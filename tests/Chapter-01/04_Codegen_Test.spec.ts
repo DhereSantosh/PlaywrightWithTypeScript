b887bbb2493e244a4986e70361417fde6dfd3811
DhereSantosh <dheresantosh9@gmai.com>
2025-06-02 11:11:14 +0530

    updated spec file

--- tests/Chapter-01/04_Codegen_Test.spec.ts
+++ tests/Chapter-01/04_Codegen_Test.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Search Existing Employee - PIM - CodegenTC', async ({ page }) => {
-  console.log('Test Execution started..');
+  console.log('Test Execution started....');
   await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
   await page.getByRole('textbox', { name: 'Username' }).click();
   await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
